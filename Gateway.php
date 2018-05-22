dc6f7639b7b2e79eb3534094cca815429ebac57b
walkor <walkor@users.noreply.github.com>
2018-05-23 01:01:23 +0800

    v3.0.9

--- Gateway.php
+++ Gateway.php
@@ -915,6 +915,25 @@ class Gateway
     }
 
     /**
+     * 取消分组
+     *
+     * @param int|string $group
+     *
+     * @return void
+     */
+    public static function ungroup($group)
+    {
+        if (!static::isValidGroupId($group)) {
+            return false;
+        }
+        $gateway_data             = GatewayProtocol::$empty;
+        $gateway_data['cmd']      = GatewayProtocol::CMD_UNGROUP;
+        $gateway_data['ext_data'] = $group;
+        return static::sendToAllGateway($gateway_data);
+
+    }
+
+    /**
      * 向所有 uid 发送
      *
      * @param int|string|array $uid
@@ -1291,10 +1310,10 @@ class Gateway
      */
     protected static function getAllGatewayAddressesFromRegister()
     {
-        static $addresses_cache, $last_update;
-        $time_now = time();
+        static $addresses_cache, $last_update, $last_register_address;
+        $time_now        = time();
         $expiration_time = 1;
-        if(empty($addresses_cache) || $time_now - $last_update > $expiration_time) {
+        if(empty($addresses_cache) || $time_now - $last_update > $expiration_time || $last_register_address !== static::$registerAddress) {
             $client = stream_socket_client('tcp://' . static::$registerAddress, $errno, $errmsg, static::$connectTimeout);
             if (!$client) {
                 throw new Exception('Can not connect to tcp://' . static::$registerAddress . ' ' . $errmsg);
@@ -1306,8 +1325,9 @@ class Gateway
                 throw new Exception('getAllGatewayAddressesFromRegister fail. tcp://' .
                     static::$registerAddress . ' return ' . var_export($ret, true));
             }
-            $last_update = $time_now;
-            $addresses_cache = $data['addresses'];
+            $last_update           = $time_now;
+            $last_register_address = static::$registerAddress;
+            $addresses_cache       = $data['addresses'];
         }
         return $addresses_cache;
     }
@@ -1455,100 +1475,72 @@ class GatewayProtocol
 {
     // 发给worker，gateway有一个新的连接
     const CMD_ON_CONNECT = 1;
-
     // 发给worker的，客户端有消息
     const CMD_ON_MESSAGE = 3;
-
     // 发给worker上的关闭链接事件
     const CMD_ON_CLOSE = 4;
-
     // 发给gateway的向单个用户发送数据
     const CMD_SEND_TO_ONE = 5;
-
     // 发给gateway的向所有用户发送数据
     const CMD_SEND_TO_ALL = 6;
-
     // 发给gateway的踢出用户
     // 1、如果有待发消息，将在发送完后立即销毁用户连接
     // 2、如果无待发消息，将立即销毁用户连接
     const CMD_KICK = 7;
-
     // 发给gateway的立即销毁用户连接
     const CMD_DESTROY = 8;
-
     // 发给gateway，通知用户session更新
     const CMD_UPDATE_SESSION = 9;
-
     // 获取在线状态
     const CMD_GET_ALL_CLIENT_SESSIONS = 10;
-
     // 判断是否在线
     const CMD_IS_ONLINE = 11;
-
     // client_id绑定到uid
     const CMD_BIND_UID = 12;
-
     // 解绑
     const CMD_UNBIND_UID = 13;
-
     // 向uid发送数据
     const CMD_SEND_TO_UID = 14;
-
     // 根据uid获取绑定的clientid
     const CMD_GET_CLIENT_ID_BY_UID = 15;
-
     // 加入组
     const CMD_JOIN_GROUP = 20;
-
     // 离开组
     const CMD_LEAVE_GROUP = 21;
-
     // 向组成员发消息
     const CMD_SEND_TO_GROUP = 22;
-
     // 获取组成员
     const CMD_GET_CLINET_SESSUONS_BY_GROUP = 23;
-
     // 获取组在线连接数
     const CMD_GET_CLIENT_COUNT_BY_GROUP = 24;
-
     // 按照条件查找
     const CMD_SELECT = 25;
-
     // 获取在线的群组ID
     const CMD_GET_GROUP_ID_LIST = 26;
-
+    // 取消分组
+    const CMD_UNGROUP = 27;
     // worker连接gateway事件
     const CMD_WORKER_CONNECT = 200;
-
     // 心跳
     const CMD_PING = 201;
-
     // GatewayClient连接gateway事件
     const CMD_GATEWAY_CLIENT_CONNECT = 202;
-
     // 根据client_id获取session
     const CMD_GET_SESSION_BY_CLIENT_ID = 203;
-
     // 发给gateway，覆盖session
     const CMD_SET_SESSION = 204;
-
     // 当websocket握手时触发，只有websocket协议支持此命令字
     const CMD_ON_WEBSOCKET_CONNECT = 205;
-
     // 包体是标量
     const FLAG_BODY_IS_SCALAR = 0x01;
-
     // 通知gateway在send时不调用协议encode方法，在广播组播时提升性能
     const FLAG_NOT_CALL_ENCODE = 0x02;
-
     /**
      * 包头长度
      *
      * @var int
      */
     const HEAD_LEN = 28;
-
     public static $empty = array(
         'cmd'           => 0,
         'local_ip'      => 0,
@@ -1561,7 +1553,6 @@ class GatewayProtocol
         'ext_data'      => '',
         'body'          => '',
     );
-
     /**
      * 返回包长度
      *
@@ -1573,11 +1564,9 @@ class GatewayProtocol
         if (strlen($buffer) < self::HEAD_LEN) {
             return 0;
         }
-
         $data = unpack("Npack_len", $buffer);
         return $data['pack_len'];
     }
-
     /**
      * 获取整个包的 buffer
      *
@@ -1600,7 +1589,6 @@ class GatewayProtocol
                 $data['flag'], $data['gateway_port'],
                 $ext_len) . $data['ext_data'] . $data['body'];
     }
-
     /**
      * 从二进制数据转换为数组
      *
